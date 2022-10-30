3f23e2841d72f0ec23bc839e0164efcda2a87fcd
poorna <srprakash016@gmail.com>
2022-10-30 12:10:53 +0530

    improving file structure

--- src/commands/folder_init.js
+++ src/commands/folder_init.js
@@ -7,62 +7,65 @@ const chalk = require('chalk');
 const { UserConfig, CheckConfig } = require('../utils/check_configfile');
 
 const createafolder = (filename, confirm = true) => {
-  if (confirm) {
-    const currentpath = shell.pwd().stdout + '/' + filename + '/';
-    if (!fs.existsSync(path.join(currentpath)) && confirm) {
-      fs.mkdirSync(path.join(currentpath));
+    if (confirm) {
+        const currentpath = shell.pwd().stdout + '/' + filename + '/';
+        if (!fs.existsSync(path.join(currentpath)) && confirm) {
+            fs.mkdirSync(path.join(currentpath));
 
-      return console.log(
-        chalk.green('Created a ') +
-          chalk.yellow(filename) +
-          chalk.green(' Folder')
-      );
-    } else {
-      return console.log(
-        chalk.red('Already Created a ') +
-          chalk.yellow(filename) +
-          chalk.green(' Folder')
-      );
+            return console.log(
+                chalk.green('Created a ') +
+                chalk.yellow(filename) +
+                chalk.green(' Folder')
+            );
+        } else {
+            return console.log(
+                chalk.red('Already Created a ') +
+                chalk.yellow(filename) +
+                chalk.green(' Folder')
+            );
+        }
     }
-  }
 };
 
 const init = (program) => {
-  const init = program
-    .command('init')
-    .description('Init will create a project structure for you')
-    .action(() => {
-      if (typeof UserConfig !== 'undefined') {
-        const foldername = UserConfig.FolderStructure;
-        createafolder(foldername.MainFolder);
-        createafolder(foldername.ComponentFolder);
-        createafolder(foldername.AssetsFolder);
-        createafolder(foldername.RouteFolder);
-        createafolder(foldername.LayoutFolder);
-        createafolder(
-          foldername.ServicesFolder.path,
-          foldername.ServicesFolder.default === 'undefined'
-            ? false
-            : foldername.ServicesFolder.default
-        );
-        createafolder(
-          foldername.PagesFolder.path,
-          foldername.PagesFolder.default === 'undefined'
-            ? false
-            : foldername.PagesFolder.default
-        );
-        createafolder(
-          foldername.ConfigFolder.path,
-          foldername.ConfigFolder.default === 'undefined'
-            ? false
-            : foldername.ConfigFolder.default
-        );
-      } else {
-        CheckConfig();
-      }
-    });
+    const init = program
+        .command('init')
+        .description('Init will create a project structure for you')
+        .action(() => {
+            const currentpath = shell.pwd().stdout;
+            console.log(currentpath);
+            console.log("object");
+            if (typeof UserConfig !== 'undefined') {
+                const foldername = UserConfig.FolderStructure;
+                createafolder(foldername.MainFolder);
+                createafolder(foldername.ComponentFolder);
+                createafolder(foldername.AssetsFolder);
+                createafolder(foldername.RouteFolder);
+                createafolder(foldername.LayoutFolder);
+                createafolder(
+                    foldername.ServicesFolder.path,
+                    foldername.ServicesFolder.default === 'undefined' ?
+                    false :
+                    foldername.ServicesFolder.default
+                );
+                createafolder(
+                    foldername.PagesFolder.path,
+                    foldername.PagesFolder.default === 'undefined' ?
+                    false :
+                    foldername.PagesFolder.default
+                );
+                createafolder(
+                    foldername.ConfigFolder.path,
+                    foldername.ConfigFolder.default === 'undefined' ?
+                    false :
+                    foldername.ConfigFolder.default
+                );
+            } else {
+                CheckConfig();
+            }
+        });
 };
 
 module.exports = {
-  init,
+    init,
 };
--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const component = require('./commands/components/components');
 const init = require('./commands/folder_init');
 
 //To check react is available
-// const toCheckReact = checker.prerequisite();
-const toCheckReact = true;
+const toCheckReact = checker.prerequisite();
+//const toCheckReact = true;
 
 if (toCheckReact) {
-  //Information about in the CLI
-  cli.infoCli(program);
+    //Information about in the CLI
+    cli.infoCli(program);
 
-  //To Create a Component in the React Folder
-  component.createcomponet(program);
-  init.init(program);
+    //To Create a Component in the React Folder
+    component.createcomponet(program);
+    init.init(program);
 }
 
 program.parse(process.argv);
